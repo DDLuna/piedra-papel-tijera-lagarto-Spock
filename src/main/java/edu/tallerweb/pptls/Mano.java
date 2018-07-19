c36ad61f66a34cc3b832cabb6f4f2695099b5d02
DDLuna <damian.daniel.luna@gmail.com>
2018-07-19 19:04:26 -0300

    solucion

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -4,24 +4,36 @@ package edu.tallerweb.pptls;
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
+	Forma forma;
 
 	/**
-	 * Toda Mano debe crearse con una forma dada, que será
-	 * la que determine su condición en el juego.
-	 * @param forma, la Forma que adopta la Mano.
+	 * Toda Mano debe crearse con una forma dada, que será la que determine su
+	 * condición en el juego.
+	 * 
+	 * @param forma,
+	 *            la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado aún");
+		this.forma = forma;
 	}
 
 	/**
-	 * Evaluará el resultado de la partida según las reglas
-	 * del juego.
-	 * @param otra, la otra Mano.
+	 * Evaluará el resultado de la partida según las reglas del juego.
+	 * 
+	 * @param otra,
+	 *            la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado aún");
+		Resultado vector[] = new Resultado[5];
+		int base = this.forma.getValor();
+		vector[base] = Resultado.EMPATA;
+		for (int i = 1; i < 3; i++) {
+			vector[(base + i) % 5] = Resultado.PIERDE;
+		}
+		for (int i = 3; i < 5; i++) {
+			vector[(base + i) % 5] = Resultado.GANA;
+		}
+		return vector[otra.forma.getValor()];
 	}
-
 }
--- src/test/java/edu/tallerweb/pptls/PartidaTests.java
+++ src/test/java/edu/tallerweb/pptls/PartidaTests.java
@@ -5,15 +5,28 @@ import org.junit.Test;
 
 public class PartidaTests {
 
+	Mano jugadorUno = new Mano(Forma.SPOCK);
+	Mano jugadorDos = new Mano(Forma.PIEDRA);
+	Mano jugadorTres = new Mano(Forma.PAPEL);
+	Mano jugadorCuatro = new Mano(Forma.TIJERA);
+	Mano jugadorCinco = new Mano(Forma.LAGARTO);
+
 	@Test
-	public void queSpockVaporizaPiedra() {
-
-		Mano jugadorUno = new Mano(Forma.SPOCK);
-		Mano jugadorDos = new Mano(Forma.PIEDRA);
-
-		assertEquals("Spock vaporiza piedra",
-				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
-
+	public void spock() {
+		assertEquals(Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+		assertEquals(Resultado.GANA, jugadorUno.jugarCon(jugadorCuatro));
+		assertEquals(Resultado.PIERDE, jugadorUno.jugarCon(jugadorTres));
+		assertEquals(Resultado.PIERDE, jugadorUno.jugarCon(jugadorCinco));
+		assertEquals(Resultado.EMPATA, jugadorUno.jugarCon(jugadorUno));
+	}
+	
+	@Test
+	public void piedra() {
+		assertEquals(Resultado.PIERDE, jugadorDos.jugarCon(jugadorUno));
+		assertEquals(Resultado.EMPATA, jugadorDos.jugarCon(jugadorDos));
+		assertEquals(Resultado.PIERDE, jugadorDos.jugarCon(jugadorTres));
+		assertEquals(Resultado.GANA, jugadorDos.jugarCon(jugadorCuatro));
+		assertEquals(Resultado.GANA, jugadorDos.jugarCon(jugadorCinco));
 	}
 
 }
